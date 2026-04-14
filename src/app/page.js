411a08444bd280e50b42db9d66c0afc11ea13473
Anil Kumar <mak97047@gmail.com>
2026-04-14 14:43:34 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center text-center p-20">Welcome to Yvity Dash Board</div>
+    <div className="flex justify-center text-center p-20">Welcome to YVITY Dash Board</div>
   );
 }
