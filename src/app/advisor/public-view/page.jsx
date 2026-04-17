2ebe5b46db146aade0e5aee5668ec6acb48cde3f
Ajay <ajay@hanvitec.com>
2026-04-17 10:07:55 +0530

    Added toggle bar in the navbar and navbar

--- src/app/advisor/public-view/page.jsx
+++ src/app/advisor/public-view/page.jsx
@@ -1,17 +1,17 @@
 "use client";
 import ProgressBar from "@/app/components/ui/Progressionbar";
 import React from "react";
-import { BsFillInfoSquareFill } from "react-icons/bs";
+import { BsChatDots, BsFillInfoSquareFill } from "react-icons/bs";
 import { CiBank } from "react-icons/ci";
 import { FaPlayCircle } from "react-icons/fa";
-import { IoIosPlay, IoMdCheckmarkCircle } from "react-icons/io";
+import { IoIosCall, IoIosPlay, IoMdCheckmarkCircle } from "react-icons/io";
 import {
   MdBarChart,
   MdCall,
   MdLocationPin,
   MdOutlineChevronLeft,
 } from "react-icons/md";
-import { TbDownload } from "react-icons/tb";
+import { TbDownload, TbMail } from "react-icons/tb";
 import { useRouter } from "next/navigation";
 
 const page = () => {
@@ -97,7 +97,7 @@ const page = () => {
   return (
     <div className="bg-[#F8F6F1]">
       {/* Header */}
-      <header className="h-[60px] bg-[#0A4A4A] w-full shadow-md">
+      {/* <header className="h-[60px] bg-[#0A4A4A] w-full shadow-md">
         <span
           className="flex items-center px-4 sm:px-6 md:pl-10 md:py-[18px] cursor-pointer"
           onClick={() => router.back()}
@@ -105,12 +105,12 @@ const page = () => {
           <MdOutlineChevronLeft />
           Back
         </span>
-      </header>
+      </header> */}
       {/* content */}
-      <div className=" flex flex-col gap-4 p-4 sm:p-6 px-4 sm:px-6 md:px-[123px] md:pb-[89px]">
+      <div className="w-full flex flex-col gap-4 p-4 sm:p-6 xl:px-[15px]">
         <div className="flex flex-col md:flex-row pt-6 w-full gap-[14px]">
           {/* left Portion */}
-          <div className="relative md:w-[891px] md:h-[725px] rounded-2xl bg-white shadow-sm">
+          <div className="relative rounded-2xl bg-white shadow-sm flex-1">
             <div className="h-[78px] bg-gradient-to-r from-[#032B2B] to-[#095A5B] mb-[65px] w-full rounded-t-2xl"></div>
             {/* profile circle */}
             <span className="absolute h-16 w-16 sm:h-20 sm:w-20 md:h-22 md:w-22 rounded-full bg-yellow-300 top-[39px] left-4 md:left-[30px]"></span>
@@ -134,7 +134,7 @@ const page = () => {
             </div>
             {/* intro video */}
             <div className="pr-[30px] pl-[29px]">
-              <div className="bg-amber-200 md:h-[59px] w-full md:w-[832px] pl-[20px] pr-[23px] py-[9px] mt-[24px] bg-gradient-to-r from-[#022927] to-[#053F40] rounded-lg">
+              <div className="bg-amber-200 w-full  pl-[20px] pr-[23px] py-[9px] mt-[24px] bg-gradient-to-r from-[#022927] to-[#053F40] rounded-lg">
                 <span className="flex gap-5 h-full">
                   <FaPlayCircle
                     size={40}
@@ -154,7 +154,7 @@ const page = () => {
             </div>
             {/* verified fields */}
             <div className="pl-[29px] pr-4 md:pr-[40px] mt-[16px]">
-              <div className="flex gap-2 ">
+              <div className="flex gap-2 items-center">
                 {aboutData.map((data, index) => (
                   <span
                     key={index}
@@ -165,7 +165,7 @@ const page = () => {
                   </span>
                 ))}
               </div>
-              <div className="md:h-[70px] md:w-[820px] rounded-lg border border-[#E2E2E2] bg-[#F0F8F8] py-[12px] pl-[18px] pr-[34px] mt-[16px] ">
+              <div className="rounded-lg border border-[#E2E2E2] bg-[#F0F8F8] py-[12px] pl-[18px] pr-[34px] mt-[16px] ">
                 <span className="flex items-center gap-[11px] ">
                   <h2 className="text-teal-950 text-2xl font-bold leading-4 font-poppins">
                     87/100
@@ -179,21 +179,21 @@ const page = () => {
                   <ProgressBar value={87} />
                 </span>
               </div>
-              <div className="flex flex-col sm:flex-row flex-wrap gap-3 mt-4">
-                {aboutData.map((item, index) => (
+              <div className="grid grid-cols-4 sm:flex-row flex-wrap gap-3 mt-4">
+                {summaryData.map((item, index) => (
                   <div
                     key={index}
-                    className="w-full sm:w-[200px] min-h-[60px] px-4 sm:px-6 py-3 
-      flex flex-col justify-center border rounded-md"
+                    className="w-full px-4 sm:px-6 py-3 
+      flex flex-col justify-center border rounded-md bg-[#F0F8F8]"
                   >
                     {/* count */}
-                    <span className="text-xs sm:text-sm font-semibold text-gray-800">
-                      {index + 1}
+                    <span className="text-heading text-center font-poppins text-base font-bold leading-[16px]">
+                      {item.count}
                     </span>
 
                     {/* label */}
-                    <span className="text-[10px] sm:text-xs text-gray-500">
-                      {item}
+                    <span className="text-secondaryLabel text-center font-poppins text-xs font-semibold">
+                      {item.label}
                     </span>
                   </div>
                 ))}
@@ -202,7 +202,7 @@ const page = () => {
                 {actions.map((item, index) => (
                   <button
                     key={index}
-                    className="min-h-[40px] px-3 py-2 border rounded-md 
+                    className="px-3 py-2 border rounded-md 
       flex items-center justify-center 
       text-xs sm:text-sm font-medium 
       hover:bg-teal-950 hover:text-white transition"
@@ -227,9 +227,9 @@ const page = () => {
             </div>
           </div>
           {/* Right potion */}
-          <div className="flex flex-col justify-between">
+          <div className="flex flex-col justify-between min-w-[289px]">
             {/* R1 */}
-            <div className="w-full md:w-[289px] md:h-[225px] py-6 px-[17px] flex flex-col gap-3 rounded-2xl bg-white shadow-soft">
+            <div className="w-full py-6 px-[17px] flex flex-col gap-3 rounded-2xl bg-white shadow-soft">
               {/* Heading */}
               <span className="text-[16px] font-bold text-[#111827] font-[Poppins] flex items-center gap-2">
                 <span>
@@ -240,22 +240,25 @@ const page = () => {
 
               {/* Buttons container */}
               <div className="flex flex-col gap-2 flex-1">
-                <button className=" h-[40px]  w-full md:w-[255px]   text-[10px] sm:text-xs md:text-xs sm:text-xs sm:text-sm md:text-sm md:text-xs sm:text-sm md:text-sm font-medium rounded-lg bg-[#0A4A4A]">
+                <button className="w-full text-[10px] px-[23px] py-[14px] font-medium rounded-lg bg-[#0A4A4A] flex gap-2 items-center justify-center text-white">
+                  <IoIosCall />
                   Call Now
                 </button>
 
-                <button className="h-[40px]  w-full md:w-[255px] text-[10px] sm:text-xs md:text-xs sm:text-xs sm:text-sm md:text-sm md:text-xs sm:text-sm md:text-sm font-medium rounded-lg bg-[#26D367]">
-                  WhatsApp
+                <button className="w-full text-[10px] px-[23px] py-[14px] font-medium rounded-lg bg-[#26D367] flex gap-2 items-center justify-center text-white">
+                  <BsChatDots />
+                  Whatsapp
                 </button>
 
-                <button className="h-[40px] w-full md:w-[255px] rounded-lg border border-[#E8F4F4] text-[10px] sm:text-xs md:text-xs sm:text-xs sm:text-sm md:text-sm md:text-xs sm:text-sm md:text-sm font-medium">
-                  Send Mail
+                <button className="w-full text-[10px] px-[23px] py-[14px] bg-white flex gap-2 items-center justify-center rounded-lg border border-[#E8F4F4] text-primary-900 font-poppins text-xs font-semibold">
+                  <TbMail />
+                  Call Now
                 </button>
               </div>
             </div>
 
             {/* R2 */}
-            <div className="w-full md:w-[289px] h-[282px] px-[17px] py-[23px] flex flex-col gap-[15px] rounded-2xl bg-white shadow-soft">
+            <div className="w-full px-[17px] py-[23px] flex flex-col gap-[15px] rounded-2xl bg-white shadow-soft">
               {/* Heading */}
               <span className="text-[var(--headings-important-text)] text-[16px] font-bold font-[Poppins] leading-normal flex items-center gap-[15px]">
                 <span>
@@ -268,7 +271,7 @@ const page = () => {
                 {statsData.map((item, index) => (
                   <div
                     key={index}
-                    className=" h-[33px] w-[255px] py-[8px] pl-5 pr-[10px] flex justify-between items-center rounded-lg bg-[#F0F8F8]"
+                    className="py-[8px] pl-5 pr-[10px] flex justify-between items-center rounded-lg bg-[#F0F8F8]"
                   >
                     <span className="text-[12px] font-normal text-[#6B7280] font-[Nunito] leading-[16px]">
                       {item.label}
@@ -282,7 +285,7 @@ const page = () => {
             </div>
 
             {/* R3 */}
-            <div className="w-full md:w-[289px] h-[174px] py-[23px] pl-[20px] pr-[53px] flex flex-col gap-3 rounded-2xl bg-white shadow-soft">
+            <div className="w-full py-[23px] pl-[20px] pr-[53px] flex flex-col gap-3 rounded-2xl bg-white shadow-soft">
               {/* Heading */}
               <span className="text-[var(--headings-important-text)] text-[16px] font-bold font-[Poppins] leading-normal flex items-center gap-2">
                 <span>
@@ -307,12 +310,12 @@ const page = () => {
           </div>
         </div>
         {/* second layer */}
-        <div className="md:h-[288px] md:w-[1198px] rounded-2xl bg-white shadow-soft">
-          <div className="border-b border-highlights rounded-t-2xl flex flex-wrap gap-2 px-2 sm:px-4 py-2">
+        <div className="rounded-2xl bg-white shadow-soft">
+          <div className="border-b border-highlights rounded-t-2xl flex gap-0">
             {footerheadings.map((heading, index) => (
               <button
                 key={index}
-                className="w-full font-poppins sm:w-[132px] h-[32px] px-3 text-center text-[10px] sm:text-xs md:text-xs sm:text-xs sm:text-sm md:text-sm md:text-xs sm:text-sm md:text-sm font-medium cursor-pointer"
+                className="font-poppins p-[10px] text-center text-[10px] cursor-pointer text-primary-900  text-sm font-bold"
               >
                 {heading}
               </button>
@@ -321,7 +324,7 @@ const page = () => {
 
           {/* Dynamic content */}
           <div className="pt-[20px] px-4 md:pl-[40px] md:pr-[240px] pb-[36px] flex flex-col gap-2 ">
-            <p className="text-[var(--labels-secondary-info)] text-[14px] font-normal font-[Nunito] leading-[24px]">
+            <p className="text-[#6B7280] font-nunito text-sm font-normal leading-6">
               I am Krishna Mohan, a Senior LIC Advisor based in Nellore, AP with
               over 14 years of experience. I am an MDRT qualifier and YVITY
               Verified Professional. My mission is to provide trusted,
@@ -336,7 +339,7 @@ const page = () => {
                 {companies.map((comp, index) => (
                   <span
                     key={index}
-                    className="w-[156px] h-[28px] p-[10px] flex gap-2 items-center text-[var(--primary-900)] text-[12px] font-semibold font-[Poppins] leading-normal rounded-2xl bg-[#E8F4F4]"
+                    className="p-[10px] flex gap-2 items-center text-[var(--primary-900)] text-[12px] font-semibold font-[Poppins] leading-normal rounded-2xl bg-[#E8F4F4]"
                   >
                     <span className="">{comp.icon}</span>
                     {comp.data}
