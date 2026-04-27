8ca488e74f6e066388337add38dfe2021f9bf13d
Ajay <ajay@hanvitec.com>
2026-04-27 11:49:28 +0530

    redirection fixed

--- src/proxy.js
+++ src/proxy.js
@@ -13,7 +13,6 @@ export function proxy(request) {
 
   if (!session || !session.token) {
     return NextResponse.redirect(new URL("https://yvity.vercel.app", request.url));
-    
   }
 
   return NextResponse.next();
